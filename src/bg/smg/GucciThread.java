1d090c360466d211bea3cc094abc79b6e03592a1
smgC109 <krisivs@abv.bg>
2024-05-16 07:58:32 +0300

    new files

---
+++ src/bg/smg/GucciThread.java
@@ -0,0 +1,16 @@
+package bg.smg;
+
+public class GucciThread implements Runnable {
+    @Override
+    public void run() {
+        for (int i = 1; i <= 100; i += 2) {
+            System.out.println("Odd: " + i);
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
+
---
+++ src/bg/smg/TrussardiThread.java
@@ -0,0 +1,15 @@
+package bg.smg;
+
+public class TrussardiThread implements Runnable {
+    @Override
+    public void run() {
+        for (int i = 1; i <= 100; i += 2) {
+            System.out.println("Odd: " + i);
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
